fd54ccbbae086886850ddd689c243698aa2b623b
msvstj <savastijonok@gmail.com>
2023-11-28 11:02:42 +0100

    fix: build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NgmiPolyfill } from 'vite-plugin-ngmi-polyfill';
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [NgmiPolyfill(), react()],
   resolve: {
     alias: {
-      '@': './src',
+      "@/": path.join(__dirname, "src/"),
     },
   },
 });
